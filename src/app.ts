5645e44f890303729d405636c69ab99db3077548
solareenlo <solareenlo@solareenlo.com>
2019-06-01 02:48:32 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello World!!!'));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  const posts = [
+    {
+      id: '123',
+      title: 'First title',
+      content: 'First content'
+    },
+    {
+      id: '456',
+      title: 'Secont title',
+      content: 'Secont content'
+    }
+  ];
+  res.status(200).json({
+    message: 'fetch success',
+    posts: posts
+  });
+});
+
+app.post('/', (req, res, next) => {
+  const post = req.body;
+  console.log(req.body);
+  res.status(200).json({
+    message: 'Post success!!!'
+  });
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import chalk from 'chalk';
 import dotenv from 'dotenv';
 import { Request, Response, NextFunction } from 'express';
+import errorHandler from 'errorhandler';
 
 import app from './app';
+
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorHandler());
+} else {
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).send('Server Error');
+  });
+}
 
 const normalizePort = (val: string) => {
   const port = parseInt(val, 10);
@@ -26,6 +37,8 @@ const server = app.listen(app.get('port'), () => {
     chalk.green('✓'),
     app.get('port')
   );
+  console.log('%s %sモードです.', chalk.green('✓'), app.get('env'));
+  console.log('%s CTRL-C で停止します.', chalk.green('✓'));
 });
 
 export default server;
